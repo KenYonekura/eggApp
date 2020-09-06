6cf2361aa1dcde930d935a8e027351b6e2a7780d
KenYonekura <p.p.high.llows@gmail.com>
2020-09-06 18:08:00 +0900

    first commit

--- eggApp/ViewController.swift
+++ eggApp/ViewController.swift
@@ -12,9 +12,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countLabel.text = String(count)
+        
     }
 
+    @IBOutlet weak var countLabel: UILabel!
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var count :Int = 100
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var dispImageNum = 0
+    
+    func dispImage() {
+        let imageArray = ["egg01","egg02","egg03",]
+        
+        let name = imageArray[dispImageNum]
+        let image = UIImage(named: name)
+        
+        imageView.image = image
+    }
+    
+    
+    @IBAction func button(_ sender: Any) {
+        
+        count -= 1
+        countLabel.text = String(count)
+        
+//        カウントに応じたテキストの変更
+        if (count == 75) {
+            textLabel.text = "まだまだ！"
+        }else if(count == 50){
+            textLabel.text = "あと半分！"
+            dispImageNum += 1
+        }else if(count == 25){
+            textLabel.text = "あと少し！"
+        } else if count == 0{
+            dispImageNum += 1
+        }
+        dispImage()
+    }
 }
 
